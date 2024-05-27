bbf91ac646a5964542476205fdc650fbb6e7d587
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-05-27 19:12:00 +0530

    Update start.py

--- JARVIS/modules/start.py
+++ JARVIS/modules/start.py
@@ -1,7 +1,7 @@
 from telethon import __version__, events, Button
 from config import X1
 
-# Define the start button layout
+# Constants
 START_BUTTON = [
     [Button.inline("• ᴄᴏᴍᴍᴀɴᴅs •", data="help_back")],
     [
@@ -11,26 +11,36 @@ START_BUTTON = [
     [Button.url("• ʀᴇᴘᴏ •", "https://github.com/doraemon890/JARVIS-X-SPAM")]
 ]
 
-# Define the start command handler
+IMAGE_URL = "https://github.com/doraemon890/JARVIS-X-SPAM/assets/155803358/f30a5777-9823-45d0-9860-342eceadb774"
+PYTHON_VERSION = "3.11.3"
+JARVIS_VERSION = "M 1.8.31"
+
+async def get_bot_info(event):
+    ANNIE = await event.client.get_me()
+    bot_name = ANNIE.first_name
+    bot_id = ANNIE.id
+    return bot_name, bot_id
+
+def create_start_text(bot_name, bot_id, sender_name, sender_id):
+    return (
+        f"**ʜᴇʏ​ [{sender_name}](tg://user?id={sender_id}),\n\n"
+        f"ɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n"
+        "━━━━━━━━━━━━━━━━━━━\n\n"
+        f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [JARVIS](https://t.me/JARVIS_V2)**\n\n"
+        f"» **ᴊᴀʀᴠɪs V2 :** `{JARVIS_VERSION}`\n"
+        f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{PYTHON_VERSION}`\n"
+        f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n"
+        "━━━━━━━━━━━━━━━━━"
+    )
+
 @X1.on(events.NewMessage(pattern="/start"))
 async def start(event):              
     if event.is_private:
-        ANNIE = await event.client.get_me()
-        bot_name = ANNIE.first_name
-        bot_id = ANNIE.id
-        TEXT = (
-            f"**ʜᴇʏ​ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\n"
-            f"ɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})​**\n"
-            "━━━━━━━━━━━━━━━━━━━\n\n"
-            f"» **ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ : [JARVIS](https://t.me/JARVIS_V2)**\n\n"
-            f"» **ᴊᴀʀᴠɪs V2 :** `M 1.8.31`\n"
-            f"» **ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `3.11.3`\n"
-            f"» **ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{__version__}`\n"
-            "━━━━━━━━━━━━━━━━━"
-        )
+        bot_name, bot_id = await get_bot_info(event)
+        start_text = create_start_text(bot_name, bot_id, event.sender.first_name, event.sender.id)
         await event.client.send_file(
             event.chat_id,
-            "https://github.com/doraemon890/JARVIS-X-SPAM/assets/155803358/f30a5777-9823-45d0-9860-342eceadb774",
-            caption=TEXT,
+            IMAGE_URL,
+            caption=start_text,
             buttons=START_BUTTON
         )
