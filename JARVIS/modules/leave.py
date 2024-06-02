260dc088689c26d7360b0c5aa88500b0df523f74
RAJNISH XD <155803358+doraemon890@users.noreply.github.com>
2024-06-03 00:39:20 +0530

    Update leave.py

--- JARVIS/modules/leave.py
+++ JARVIS/modules/leave.py
@@ -12,13 +12,13 @@ async def leave_group(event, group_id):
 async def leave(e):
     if e.sender_id in SUDO_USERS:
         if len(e.text) > 7:
-            event = await e.reply("» GROUP CHOR KE JAA RHA HU...")
+            event = await e.reply("» ʟᴇᴀᴠɪɴɢ ᴛʜᴇ ɢʀᴏᴜᴘ...")
             mkl = e.text.split(" ", 1)
             await leave_group(event, mkl[1])
         else:
             if e.is_private:
-                alt = f"**» ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ !!**\n\n» {hl}leave <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n» {hl}leave : ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
+                alt = f"**» ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ**\n\n» {hl}leave <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n» {hl}leave : ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
                 await e.reply(alt)
             else:
-                event = await e.reply("» GROUP CHOR KE JAA RHA HU...")
+                event = await e.reply("» ʟᴇᴀᴠɪɴɢ ᴛʜᴇ ɢʀᴏᴜᴘ...")
                 await leave_group(event, e.chat_id)
